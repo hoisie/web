6e31995fe4a6c9d2562de386f8a9b6fce922ae0e
Michael Hoisie <hoisie@scribd.com>
2009-12-15 16:35:05 -0800

    gofmt -spaces -tabwidth=4

--- web.go
+++ web.go
@@ -1,112 +1,112 @@
 package web
 
 import (
-	"bytes";
-	"fmt";
-	"http";
-	"io/ioutil";
-	"log";
-	"os";
-	"reflect";
-	"regexp";
-	"strings";
-	"template";
+    "bytes";
+    "fmt";
+    "http";
+    "io/ioutil";
+    "log";
+    "os";
+    "reflect";
+    "regexp";
+    "strings";
+    "template";
 )
 
 var compiledRoutes = map[*regexp.Regexp]*reflect.FuncValue{}
 
 func compileRoutes(urls map[string]interface{}) {
-	for r, f := range urls {
-		regex, err := regexp.Compile(r);
-		if err != nil {
-			println("Error in route")
-		}
-		fv := reflect.NewValue(f).(*reflect.FuncValue);
-		compiledRoutes[regex] = fv;
-	}
+    for r, f := range urls {
+        regex, err := regexp.Compile(r);
+        if err != nil {
+            println("Error in route")
+        }
+        fv := reflect.NewValue(f).(*reflect.FuncValue);
+        compiledRoutes[regex] = fv;
+    }
 }
 
 func routeHandler(c *http.Conn, req *http.Request) {
-	println(req.RawURL);
+    println(req.RawURL);
 
-	//try to serve a static file
-	if strings.HasPrefix(req.RawURL, "/static/") {
-		staticFile := req.RawURL[8:];
-		if len(staticFile) > 0 {
-			http.ServeFile(c, req, "static/"+staticFile)
-		}
-	}
+    //try to serve a static file
+    if strings.HasPrefix(req.RawURL, "/static/") {
+        staticFile := req.RawURL[8:];
+        if len(staticFile) > 0 {
+            http.ServeFile(c, req, "static/"+staticFile)
+        }
+    }
 
-	var route string = req.RawURL;
-	for r, fv := range compiledRoutes {
-		if !r.MatchString(route) {
-			continue
-		}
-		match := r.MatchStrings(route);
-		if len(match) > 0 {
-			if len(match[0]) != len(route) {
-				continue
-			}
-			args := make([]reflect.Value, len(match)-1);
+    var route string = req.RawURL;
+    for r, fv := range compiledRoutes {
+        if !r.MatchString(route) {
+            continue
+        }
+        match := r.MatchStrings(route);
+        if len(match) > 0 {
+            if len(match[0]) != len(route) {
+                continue
+            }
+            args := make([]reflect.Value, len(match)-1);
 
-			expectedIn := fv.Type().(*reflect.FuncType).NumIn();
-			actualIn := len(match) - 1;
+            expectedIn := fv.Type().(*reflect.FuncType).NumIn();
+            actualIn := len(match) - 1;
 
-			if expectedIn != actualIn {
-				message := fmt.Sprintf("%s - Incorrect number of arguments", req.RawURL);
-				println(message);
-				return;
-			}
+            if expectedIn != actualIn {
+                message := fmt.Sprintf("%s - Incorrect number of arguments", req.RawURL);
+                println(message);
+                return;
+            }
 
-			for i, arg := range match[1:] {
-				args[i] = reflect.NewValue(arg)
-			}
-			ret := fv.Call(args)[0].(*reflect.StringValue).Get();
-			var buf bytes.Buffer;
-			buf.WriteString(ret);
-			c.Write(buf.Bytes());
-		}
-	}
+            for i, arg := range match[1:] {
+                args[i] = reflect.NewValue(arg)
+            }
+            ret := fv.Call(args)[0].(*reflect.StringValue).Get();
+            var buf bytes.Buffer;
+            buf.WriteString(ret);
+            c.Write(buf.Bytes());
+        }
+    }
 
 }
 
-func render(tmplString string, context interface {} ) (string, os.Error) {
+func render(tmplString string, context interface{}) (string, os.Error) {
 
-	var tmpl *template.Template;
-  var err os.Error;
-  
-	if tmpl, err = template.Parse(tmplString, nil); err != nil {
-		return "", err;
-	}
+    var tmpl *template.Template;
+    var err os.Error;
 
-	var buf bytes.Buffer;
+    if tmpl, err = template.Parse(tmplString, nil); err != nil {
+        return "", err
+    }
 
-	tmpl.Execute(context, &buf);
-	return buf.String(), nil;  
+    var buf bytes.Buffer;
+
+    tmpl.Execute(context, &buf);
+    return buf.String(), nil;
 }
 
 
 func RenderFile(filename string, context interface{}) (string, os.Error) {
-	var templateBytes []uint8;
-	var err os.Error;
+    var templateBytes []uint8;
+    var err os.Error;
 
-	if templateBytes, err = ioutil.ReadFile(filename); err != nil {
-		return "", err
-	}
+    if templateBytes, err = ioutil.ReadFile(filename); err != nil {
+        return "", err
+    }
 
-  return render( string(templateBytes), context);
+    return render(string(templateBytes), context);
 }
 
-func RenderString(tmplString string, context interface {} ) (string, os.Error) {
-  return render(tmplString, context);
+func RenderString(tmplString string, context interface{}) (string, os.Error) {
+    return render(tmplString, context)
 }
 
 func Run(urls map[string]interface{}, addr string) {
-	compileRoutes(urls);
-	http.Handle("/", http.HandlerFunc(routeHandler));
+    compileRoutes(urls);
+    http.Handle("/", http.HandlerFunc(routeHandler));
 
-	err := http.ListenAndServe(addr, nil);
-	if err != nil {
-		log.Exit("ListenAndServe:", err)
-	}
+    err := http.ListenAndServe(addr, nil);
+    if err != nil {
+        log.Exit("ListenAndServe:", err)
+    }
 }
--- webgo.go
+++ webgo.go
@@ -1,155 +1,155 @@
 package main
 
 import (
-	"bytes";
-	"fmt";
-	"ini";
-	"io";
-	"io/ioutil";
-	"os";
-	"path";
+    "bytes";
+    "fmt";
+    "ini";
+    "io";
+    "io/ioutil";
+    "os";
+    "path";
 )
 
 func writeFile(contents string, filename string) os.Error {
-	var buf bytes.Buffer;
+    var buf bytes.Buffer;
 
-	buf.WriteString(contents);
+    buf.WriteString(contents);
 
-	if err := ioutil.WriteFile(filename, buf.Bytes(), 0644); err != nil {
-		return err
-	}
+    if err := ioutil.WriteFile(filename, buf.Bytes(), 0644); err != nil {
+        return err
+    }
 
-	return nil;
+    return nil;
 }
 
-func printHelp()	{ println("Commands: create, serve") }
+func printHelp() { println("Commands: create, serve") }
 
 func exists(path string) bool {
-	_, err := os.Lstat(path);
-	return err == nil;
+    _, err := os.Lstat(path);
+    return err == nil;
 }
 
 func createProject(name string) {
-	cwd := os.Getenv("PWD");
-	projectDir := path.Join(cwd, name);
+    cwd := os.Getenv("PWD");
+    projectDir := path.Join(cwd, name);
 
-	if exists(projectDir) {
-		println("Project directory already exists");
-		os.Exit(0);
-	}
+    if exists(projectDir) {
+        println("Project directory already exists");
+        os.Exit(0);
+    }
 
-	println("Creating directory ", projectDir);
-	if err := os.Mkdir(projectDir, 0744); err != nil {
-		println(err.String());
-		os.Exit(0);
-	}
+    println("Creating directory ", projectDir);
+    if err := os.Mkdir(projectDir, 0744); err != nil {
+        println(err.String());
+        os.Exit(0);
+    }
 
-	appfile := path.Join(projectDir, name+".go");
-	appcontents := fmt.Sprintf(apptmpl, name);
-	println("Creating application file", appfile);
-	writeFile(appcontents, appfile);
+    appfile := path.Join(projectDir, name+".go");
+    appcontents := fmt.Sprintf(apptmpl, name);
+    println("Creating application file", appfile);
+    writeFile(appcontents, appfile);
 
-	inifile := path.Join(projectDir, "default.ini");
-	inicontents := fmt.Sprintf(initmpl, name);
-	println("Creating config file", inifile);
-	writeFile(inicontents, inifile);
+    inifile := path.Join(projectDir, "default.ini");
+    inicontents := fmt.Sprintf(initmpl, name);
+    println("Creating config file", inifile);
+    writeFile(inicontents, inifile);
 
 }
 
 func getOutput(command string, args []string) (string, os.Error) {
-	r, w, err := os.Pipe();
-	if err != nil {
-		return "", err
-	}
-	args2 := make([]string, len(args)+1);
-	args2[0] = command;
-	copy(args2[1:], args);
-	pid, err := os.ForkExec(command, args2, os.Environ(), "", []*os.File{nil, w, w});
+    r, w, err := os.Pipe();
+    if err != nil {
+        return "", err
+    }
+    args2 := make([]string, len(args)+1);
+    args2[0] = command;
+    copy(args2[1:], args);
+    pid, err := os.ForkExec(command, args2, os.Environ(), "", []*os.File{nil, w, w});
 
-	if err != nil {
-		return "", err
-	}
+    if err != nil {
+        return "", err
+    }
 
-	w.Close();
+    w.Close();
 
-	var b bytes.Buffer;
-	io.Copy(&b, r);
-	output := b.String();
-	os.Wait(pid, 0);
+    var b bytes.Buffer;
+    io.Copy(&b, r);
+    output := b.String();
+    os.Wait(pid, 0);
 
-	return output, nil;
+    return output, nil;
 }
 
 func serveProject(inifile string) {
-	cwd := os.Getenv("PWD");
-	inifile = path.Join(cwd, inifile);
-	datadir := path.Join(cwd, "data/");
+    cwd := os.Getenv("PWD");
+    inifile = path.Join(cwd, inifile);
+    datadir := path.Join(cwd, "data/");
 
-	if !exists(datadir) {
-		if err := os.Mkdir(datadir, 0744); err != nil {
-			println(err.String());
-			os.Exit(0);
-		}
-	}
+    if !exists(datadir) {
+        if err := os.Mkdir(datadir, 0744); err != nil {
+            println(err.String());
+            os.Exit(0);
+        }
+    }
 
-	config, err := ini.ParseFile(inifile);
+    config, err := ini.ParseFile(inifile);
 
-	if err != nil {
-		println("Error parsing config", err.String())
-	}
+    if err != nil {
+        println("Error parsing config", err.String())
+    }
 
-	app := config["main"]["application"];
+    app := config["main"]["application"];
 
-	println("Serving application", app);
+    println("Serving application", app);
 
-	address := fmt.Sprintf("%s:%s", config["main"]["bind_address"], config["main"]["port"]);
-	gobin := os.Getenv("GOBIN");
+    address := fmt.Sprintf("%s:%s", config["main"]["bind_address"], config["main"]["port"]);
+    gobin := os.Getenv("GOBIN");
 
-	compiler := path.Join(gobin, "8g");
-	println("compiler", compiler);
+    compiler := path.Join(gobin, "8g");
+    println("compiler", compiler);
 
-	if err != nil {
-		println("Error parsing config", err.String())
-	} else {
-		println(address)
-	}
+    if err != nil {
+        println("Error parsing config", err.String())
+    } else {
+        println(address)
+    }
 
-	appSrc := path.Join(cwd, app+".go");
-	appObj := path.Join(datadir, app+".8");
+    appSrc := path.Join(cwd, app+".go");
+    appObj := path.Join(datadir, app+".8");
 
-	compoutput, err := getOutput(compiler, []string{"-o", appObj, appSrc});
+    compoutput, err := getOutput(compiler, []string{"-o", appObj, appSrc});
 
-	if err == nil {
-		println("Compiling", compoutput)
-	}
+    if err == nil {
+        println("Compiling", compoutput)
+    }
 
-	//8g -o data/hello.8 hello.go
-	//8g -I data runner.go
-	//8l -o data/hello data/hello.8 data/runner.8
+    //8g -o data/hello.8 hello.go
+    //8g -I data runner.go
+    //8l -o data/hello data/hello.8 data/runner.8
 
 }
 
 func main() {
-	if len(os.Args) <= 1 {
-		printHelp();
-		os.Exit(0);
-	}
+    if len(os.Args) <= 1 {
+        printHelp();
+        os.Exit(0);
+    }
 
-	command := os.Args[1];
+    command := os.Args[1];
 
-	switch command {
-	case "create":
-		createProject(os.Args[2])
+    switch command {
+    case "create":
+        createProject(os.Args[2])
 
-	case "serve":
-		serveProject(os.Args[2])
+    case "serve":
+        serveProject(os.Args[2])
 
-	case "help":
-		printHelp()
+    case "help":
+        printHelp()
 
-	default:
-		printHelp()
-	}
+    default:
+        printHelp()
+    }
 }
 
 var apptmpl = `package %s

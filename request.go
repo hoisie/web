40618a7a97861837aa30a64105caf775af9e827f
Michael Hoisie <hoisie@scribd.com>
2010-01-05 00:51:58 -0800

    Initial multipart form data parser

--- request.go
+++ request.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+    "bytes"
     "container/vector"
     "fmt"
     "http"
@@ -25,6 +26,7 @@ type Request struct {
     UserAgent  string
     Params     map[string][]string
     Cookies    map[string]string
+    Files      map[string][]byte
 }
 
 type badStringError struct {
@@ -144,7 +146,58 @@ func (r *Request) ParseParams() (err os.Error) {
                 return err
             }
             query = string(b)
-        // TODO(dsymonds): Handle multipart/form-data
+        case "multipart/form-data":
+            r.Files = make(map[string][]byte)
+            boundary := strings.Split(ct, "boundary=", 2)[1]
+            var b []byte
+            if b, err = ioutil.ReadAll(r.Body); err != nil {
+                return err
+            }
+            parts := bytes.Split(b, strings.Bytes("--"+boundary+"\r\n"), 0)
+            for _, data := range (parts) {
+                if len(data) == 0 {
+                    continue
+                }
+                var line []byte
+                var rest = data
+                headers := map[string]string{}
+                isfile := false
+                var name string
+                for {
+                    res := bytes.Split(rest, []byte{'\r', '\n'}, 2)
+                    if len(res) != 2 {
+                        break
+                    }
+                    line = res[0]
+                    rest = res[1]
+                    if len(line) == 0 {
+                        break
+                    }
+
+                    header := strings.Split(string(line), ":", 2)
+                    n := strings.TrimSpace(header[0])
+                    v := strings.TrimSpace(header[1])
+                    if n == "Content-Disposition" {
+                        parts := strings.Split(v, ";", 0)
+                        for _, parm := range (parts[1:]) {
+                            pp := strings.Split(parm, "=", 2)
+                            pn := strings.TrimSpace(pp[0])
+                            pv := strings.TrimSpace(pp[1])
+                            if pn == "name" {
+                                name = pv[1 : len(pv)-1]
+                            } else if pn == "filename" {
+                                isfile = true
+                            }
+                        }
+                    }
+
+                    headers[n] = v
+                }
+                if isfile {
+                    parts = bytes.Split(rest, strings.Bytes("\r\n--"+boundary+"--\r\n"), 0)
+                    r.Files[name] = parts[0]
+                }
+            }
         default:
             return &badStringError{"unknown Content-Type", ct}
         }
@@ -174,6 +227,16 @@ func (r *Request) ParseCookies() (err os.Error) {
 }
 
 func (r *Request) HasParam(name string) bool {
+    if r.Params == nil || len(r.Params) == 0 {
+        return false
+    }
     _, ok := r.Params[name]
+    return ok
+}
+func (r *Request) HasFile(name string) bool {
+    if r.Files == nil || len(r.Files) == 0 {
+        return false
+    }
+    _, ok := r.Files[name]
     return ok
 }

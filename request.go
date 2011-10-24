f197debd938af75883b0b1e919fb1b56e6baa50e
Michael Hoisie <hoisie@gmail.com>
2011-10-24 13:12:48 -0400

    Fix for release.r60.3

--- request.go
+++ request.go
@@ -195,10 +195,7 @@ func (r *Request) parseParams() (err os.Error) {
             r.Params = map[string]string{}
             json.Unmarshal(b, r.Params)
         case "multipart/form-data":
-            _, params, err := mime.ParseMediaType(ct)
-            if err != nil {
-                return err
-            }
+            _, params := mime.ParseMediaType(ct)
             boundary, ok := params["boundary"]
             if !ok {
                 return os.NewError("Missing Boundary")
@@ -224,10 +221,7 @@ func (r *Request) parseParams() (err os.Error) {
                     continue
                 }
                 name := part.FormName()
-                d, params, err := mime.ParseMediaType(v)
-                if err != nil {
-                    return err
-                }
+                d, params := mime.ParseMediaType(v)
                 if d != "form-data" {
                     continue
                 }

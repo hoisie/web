9100ee3e37995293be2be453bb10b1fb90580ee5
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2012-04-02 13:59:45 -0400

    further editing directory structure for examples to compile using the go tool

---
+++ src/github.com/hoisie/web.go/examples/hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/hoisie/web.go"
+)
+
+func hello(val string) string { return "hello " + val }
+
+func main() {
+	web.Get("/(.*)", hello)
+	web.Run("0.0.0.0:9999")
+}

c5a5d584ccd1b0883e7d4fdfc157962446fc9e3f
Ryan Day <ryan.day@cirrusworks.net>
2012-07-30 00:20:32 +0000

    Add comments

--- modules.go
+++ modules.go
@@ -8,6 +8,17 @@ import (
 	"strings"
 )
 
+/**
+ Attempts to encode the response according to the client's Accept-Encoding
+ header. If there is an error, or if the encoding requests aren't supported
+ then the original content is returned.
+
+ Encoding type:
+  * deflate (zlib stream)
+  * gzip
+
+ This should be the last module loaded
+*/
 func EncodeResponse(ctx *Context, content []byte) ([]byte, error) {
 	var compressed bytes.Buffer
 	var output io.WriteCloser

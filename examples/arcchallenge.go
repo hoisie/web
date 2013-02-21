d20c6720e619346afb12436f3cbbf0676214125a
Alexander Rødseth <rodseth@gmail.com>
2013-02-21 15:22:18 +0100

    Merge

--- examples/arcchallenge.go
+++ examples/arcchallenge.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-<<<<<<< HEAD
-	"github.com/hoisie/web"
-	"math/rand"
-	"strconv"
-	"time"
-=======
-    "github.com/rday/web"
     "strconv"
     "time"
     "math/rand"
->>>>>>> e5b7935f2c120e1ca11105c8648365017d58b0be
+
+    "github.com/rday/web"
 )
 
 var form = `<form action="say" method="POST"><input name="said"><input type="submit"></form>`
@@ -19,22 +13,6 @@ var form = `<form action="say" method="POST"><input name="said"><input type="sub
 var users = map[string]string{}
 
 func main() {
-<<<<<<< HEAD
-	rand.Seed(time.Now().UTC().UnixNano())
-	web.Config.CookieSecret = "7C19QRmwf3mHZ9CPAaPQ0hsWeufKd"
-	web.Get("/said", func() string { return form })
-	web.Post("/say", func(ctx *web.Context) string {
-		uid := strconv.FormatInt(rand.Int63(), 10)
-		ctx.SetSecureCookie("user", uid, 3600)
-		users[uid] = ctx.Params["said"]
-		return `<a href="/final">Click Here</a>`
-	})
-	web.Get("/final", func(ctx *web.Context) string {
-		uid, _ := ctx.GetSecureCookie("user")
-		return "You said " + users[uid]
-	})
-	web.Run("0.0.0.0:9999")
-=======
     rand.Seed(time.Now().UnixNano())
     web.Config.CookieSecret = "7C19QRmwf3mHZ9CPAaPQ0hsWeufKd"
     web.Get("/said", func() string { return form })
@@ -49,5 +27,4 @@ func main() {
         return "You said " + users[uid]
     })
     web.Run("0.0.0.0:9999")
->>>>>>> e5b7935f2c120e1ca11105c8648365017d58b0be
 }

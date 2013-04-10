2832452d9ca1038a0775fbd15ba768edec3501d9
Michael Hoisie <hoisie@gmail.com>
2013-04-10 09:01:45 -0700

    Updated API docs

--- makedocs.rb
+++ makedocs.rb
@@ -1,34 +1,20 @@
 #!/usr/bin/env ruby
 
-head = <<-HEAD
-<!DOCTYPE html>
-<head>
-  <link rel="stylesheet" type="text/css" href="stylesheets/styles.css">
-  <meta charset='utf-8'> 
-</head>
+header = <<-HEAD
+---
+layout: default
+title: Web.go API docs
+---
 
-<!--  Style overrrides -->
-<style>
-  dl dd {
-    font-style: normal;
-  }
-  #api {
-    padding-left: 20px;
-  }
-</style>
-
-<body>
 <div id="api">
 HEAD
 
-tail = <<-TAIL
+footer = <<-FOOT
 </div>
-</body>
-</html>
-TAIL
+FOOT
 
 f = File.open("api.html", "w")
-f.write(head)
+f.write(header)
 f.write(%x(godoc -html github.com/hoisie/web))
-f.write(tail)
+f.write(footer)
 f.close

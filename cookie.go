ac9539147eb2bf34ebd818680f24889ed6d77239
Hraban Luyat <hraban@0brg.net>
2013-04-17 07:42:34 +0200

    organize: cookie related code to cookie.go

---
+++ cookie.go
@@ -0,0 +1,96 @@
+package web
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Set a cookie with an explicit path. Duration is the cookie time-to-live in
+// seconds (0 = forever).
+func (ctx *Context) SetCookiePath(name, value string, age int64, path string) {
+	var utctime time.Time
+	if age == 0 {
+		// 2^31 - 1 seconds (roughly 2038)
+		utctime = time.Unix(2147483647, 0)
+	} else {
+		utctime = time.Unix(time.Now().Unix()+age, 0)
+	}
+	cookie := http.Cookie{Name: name, Value: value, Expires: utctime, Path: path}
+	ctx.SetHeader("Set-Cookie", cookie.String(), false)
+}
+
+// Sets a cookie -- duration is the amount of time in seconds. 0 = forever
+func (ctx *Context) SetCookie(name, value string, age int64) {
+	ctx.SetCookiePath(name, value, age, "")
+}
+
+func getCookieSig(key string, val []byte, timestamp string) string {
+	hm := hmac.New(sha1.New, []byte(key))
+
+	hm.Write(val)
+	hm.Write([]byte(timestamp))
+
+	hex := fmt.Sprintf("%02x", hm.Sum(nil))
+	return hex
+}
+
+func (ctx *Context) SetSecureCookiePath(name, val string, age int64, path string) {
+	// base64 encode the value
+	if len(ctx.Server.Config.CookieSecret) == 0 {
+		ctx.Server.Logger.Println("Secret Key for secure cookies has not been set. Please assign a cookie secret to web.Config.CookieSecret.")
+		return
+	}
+	var buf bytes.Buffer
+	encoder := base64.NewEncoder(base64.StdEncoding, &buf)
+	encoder.Write([]byte(val))
+	encoder.Close()
+	vs := buf.String()
+	vb := buf.Bytes()
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	sig := getCookieSig(ctx.Server.Config.CookieSecret, vb, timestamp)
+	cookie := strings.Join([]string{vs, timestamp, sig}, "|")
+	ctx.SetCookiePath(name, cookie, age, path)
+}
+
+func (ctx *Context) SetSecureCookie(name, val string, age int64) {
+	ctx.SetSecureCookiePath(name, val, age, "")
+}
+
+func (ctx *Context) GetSecureCookie(name string) (string, bool) {
+	for _, cookie := range ctx.Request.Cookies() {
+		if cookie.Name != name {
+			continue
+		}
+
+		parts := strings.SplitN(cookie.Value, "|", 3)
+
+		val := parts[0]
+		timestamp := parts[1]
+		sig := parts[2]
+
+		if getCookieSig(ctx.Server.Config.CookieSecret, []byte(val), timestamp) != sig {
+			return "", false
+		}
+
+		ts, _ := strconv.ParseInt(timestamp, 0, 64)
+
+		if time.Now().Unix()-31*86400 > ts {
+			return "", false
+		}
+
+		buf := bytes.NewBufferString(val)
+		encoder := base64.NewDecoder(base64.StdEncoding, buf)
+
+		res, _ := ioutil.ReadAll(encoder)
+		return string(res), true
+	}
+	return "", false
+}

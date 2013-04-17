69040876cea28e44f82709ceac4712ece483e265
Michael Hoisie <hoisie@meraki.com>
2013-04-17 13:58:13 -0700

    Update github.com/hoisie/web before rebuilding docs

--- makedocs.rb
+++ makedocs.rb
@@ -13,6 +13,9 @@ footer = <<-FOOT
 </div>
 FOOT
 
+puts "Updating github.com/hoisie/web"
+`go get -u github.com/hoisie/web`
+
 f = File.open("api.html", "w")
 f.write(header)
 f.write(%x(godoc -html github.com/hoisie/web))

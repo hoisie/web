acb29a5568f285dfc0b9675122ee4351bc264518
Hraban Luyat <hraban@0brg.net>
2013-04-18 18:16:43 +0200

    Return error if Run fails + more reorganizations
    run.go is a mess this commit is part of cleaning it up but it still
    needs a lifting (esp the tls code holy moly)

--- run.go
+++ run.go
@@ -2,86 +2,87 @@ package web
 
 import (
 	"crypto/tls"
-	"log"
 	"net"
 	"net/http"
 	"net/http/pprof"
 )
 
-func (s *Server) createServeMux(addr string) (*http.ServeMux, error) {
+// Create a http.Handler from this server that also provides some debug
+// resources
+func (s *Server) createDebugHandler() http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
 	mux.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
 	mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
 	mux.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
 	mux.Handle("/", s)
-
-	l, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatal("Listen:", err)
-	}
-	s.l = l
-
-	return mux, err
+	return mux
 }
 
-//Runs the web application and serves http requests
-func (s *Server) Run(addr string) {
+// Runs the web application and serves http requests
+func (s *Server) Run(addr string) error {
 	s.initServer()
-
-	mux := http.NewServeMux()
-	mux.Handle("/", s)
-
-	s.Logger.Printf("web.go serving %s\n", addr)
-
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Fatal("ListenAndServe:", err)
+		s.Logger.Print("run web.go: ", err)
+		return err
 	}
+	defer l.Close()
+	s.Logger.Printf("web.go serving %s\n", addr)
 	s.l = l
-	err = http.Serve(s.l, mux)
-	s.l.Close()
+	return http.Serve(l, s)
 }
 
 //Runs the secure web application and serves https requests
 func (s *Server) RunSecure(addr string, config tls.Config) error {
 	s.initServer()
-	mux := http.NewServeMux()
-	mux.Handle("/", s)
-
 	l, err := tls.Listen("tcp4", addr, &config)
 	if err != nil {
+		s.Logger.Print("run web.go: ", err)
+		return err
+	}
+	defer l.Close()
+	s.l = l
+	return http.Serve(l, s)
+}
+
+func (s *Server) RunTLS(addr string, cert string, key string) error {
+	s.initServer()
+	debugh := s.createDebugHandler()
+	s.Logger.Print("web.go serving ", addr)
+	err := http.ListenAndServeTLS(addr, cert, key, debugh)
+	if err != nil {
+		s.Logger.Print("ListenAndServe: ", err)
+		return err
+	}
+	return nil
+}
+
+func (s *Server) runTLS(addr, certFile, keyFile string) error {
+	s.initServer()
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		s.Logger.Print("start web.go: ", err)
+		return err
+	}
+	defer l.Close()
+	s.l = l
+	debugh := s.createDebugHandler()
+	s.Logger.Print("web.go serving with TLS ", addr)
+
+	srv := &http.Server{Handler: debugh}
+
+	config := &tls.Config{}
+	config.Certificates = make([]tls.Certificate, 1)
+	config.Certificates[0], err = tls.LoadX509KeyPair(certFile, keyFile)
+
+	if err != nil {
+		s.Logger.Print("TLS error: ", err)
 		return err
 	}
 
-	s.l = l
-	return http.Serve(s.l, mux)
-}
-func (s *Server) RunTLS(addr string, cert string, key string) {
-	s.initServer()
-
-	mux := http.NewServeMux()
-	mux.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
-	mux.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
-	mux.Handle("/debug/pprof/heap", pprof.Handler("heap"))
-	mux.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
-	mux.Handle("/", s)
-
-	s.Logger.Printf("web.go serving %s\n", addr)
-	/*
-	   l, err := net.Listen("tcp", addr)
-	   if err != nil {
-	       log.Fatal("ListenAndServe:", err)
-	   }
-	   s.l = l
-	   err = http.Serve(s.l, mux)
-	   s.l.Close()
-	*/
-	err := http.ListenAndServeTLS(addr, cert, key, mux)
-	if err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
-
+	tlsListener := tls.NewListener(s.l, config)
+	return srv.Serve(tlsListener)
 }
 
 //Runs the web application and serves http requests
@@ -92,27 +93,6 @@ func Run(addr string) {
 //Runs the secure web application and serves https requests
 func RunSecure(addr string, config tls.Config) {
 	mainServer.RunSecure(addr, config)
-}
-
-func (s *Server) runTLS(addr, certFile, keyFile string) {
-	s.initServer()
-
-	mux, err := s.createServeMux(addr)
-	s.Logger.Printf("web.go serving with TLS %s\n", addr)
-
-	srv := &http.Server{Handler: mux}
-
-	config := &tls.Config{}
-	config.Certificates = make([]tls.Certificate, 1)
-	config.Certificates[0], err = tls.LoadX509KeyPair(certFile, keyFile)
-
-	if err != nil {
-		log.Fatal("TLS error:", err)
-	}
-
-	tlsListener := tls.NewListener(s.l, config)
-	err = srv.Serve(tlsListener)
-	s.l.Close()
 }
 
 func RunTLS(addr, certFile, keyFile string) {

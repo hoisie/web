8d27d5a43664dc7219c260d3c4ea5c940a28fd7b
Hraban Luyat <hraban@0brg.net>
2013-04-23 14:08:31 +0200

    actually store user data in secure cookie
    more succinct example code for secure cookie

--- examples/arcchallenge.go
+++ examples/arcchallenge.go
@@ -5,41 +5,32 @@
 package main
 
 import (
-	"math/rand"
-	"strconv"
-	"time"
-
 	"github.com/hraban/web"
 )
 
-var users = map[string]string{}
-
-func root() string {
-	return `store something in a secure cookie: 
+const form = `store something in a secure cookie: 
 <form action="/say" method="POST">
   <input name="said">
   <input type="submit" value="go">
 </form>`
+
+func root(ctx *web.Context) string {
+	msg := form
+	if said, ok := ctx.GetSecureCookie("said"); ok {
+		msg = "You said " + said + "<p>" + msg
+	}
+	return msg
 }
 
-func say(ctx *web.Context) string {
-	uid := strconv.FormatInt(rand.Int63(), 10)
-	ctx.SetSecureCookie("user", uid, 3600)
-	users[uid] = ctx.Params["said"]
-	ctx.Redirect(303, "/final")
-	return `<a href="/final">Click Here</a>`
-}
-
-func final(ctx *web.Context) string {
-	uid, _ := ctx.GetSecureCookie("user")
-	return "You said " + users[uid]
+func say(ctx *web.Context) {
+	ctx.SetSecureCookie("said", ctx.Params["said"], 3600)
+	ctx.Redirect(303, "/")
+	return
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
 	web.Config.CookieSecret = "7C19QRmwf3mHZ9CPAaPQ0hsWeufKd"
 	web.Get("/", root)
 	web.Post("/say", say)
-	web.Get("/final", final)
 	web.Run("127.0.0.1:9999")
 }

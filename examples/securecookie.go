a66b426fef9abd1ac62e1b778f2f2fc64e230357
Hraban Luyat <hraban@0brg.net>
2013-04-23 14:16:55 +0200

    Documentation for securecookie.go example

--- examples/securecookie.go
+++ examples/securecookie.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+// Sample web.go application that stores user data in a secure cookie
 package main
 
 import (

57ed2ca9cd6513d1f571e7739cd926a2e4d2908f
Hraban Luyat <hraban@0brg.net>
2013-05-07 20:18:06 +0200

    Wrapper that guesses content-type from path
    resource ending in .json gets content-type application/json, .txt gets
    text/plain; charset=utf-8, etc.
    
    Not registered by default
    
    fixes hraban/web#11

---
+++ detectmimetype.go
@@ -0,0 +1,26 @@
+// Copyright © 2009--2013 The Web.go Authors
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package web
+
+import (
+	"path"
+)
+
+// Guess the mime type of the response based on the request path if not
+// explicitly set by the handler
+func GuessMimetypeWrapper(h SimpleHandler, ctx *Context) error {
+	ctx.Response.AddAfterHeaderFunc(func(w *ResponseWriter) {
+		// dont override existing
+		if w.Header().Get("content-type") != "" {
+			return
+		}
+		if ext := path.Ext(ctx.Request.URL.Path); ext != "" {
+			ctx.ContentType(ext)
+		}
+		// if that didnt work let the http package sort it out its got
+		// reasonable heuristics
+	})
+	return h(ctx)
+}

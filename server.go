44fae2087bb838d5d4a3d2d1404df0794668285c
Pierre Baillet <pierre@baillet.name>
2013-06-27 20:09:58 +0200

    More fixes

--- server.go
+++ server.go
@@ -263,14 +263,15 @@ func (s *Server) tryServingFile(name string, req *http.Request, w http.ResponseW
     return false
 }
 
-func (s *Server) LogRequest(ctx Context, sTime time.Time) {
+func (s *Server) logRequest(ctx Context, sTime time.Time) {
     //log the request
     var logEntry bytes.Buffer
     req := ctx.Request
     requestPath := req.URL.Path
 
     duration := time.Now().Sub(sTime)
-    fmt.Fprintf(&logEntry, "%s - \033[32;1m%s - %s\033[0m - %v", strings.Split(req.RemoteAddr, ":")[0], req.Method, requestPath, duration)
+    client := strings.Split(req.RemoteAddr, ":")
+    fmt.Fprintf(&logEntry, "%s - \033[32;1m %s - %s\033[0m - %v", strings.Join(client[0:len(client)-1], ":") , req.Method, requestPath, duration)
 
     if len(ctx.Params) > 0 {
         fmt.Fprintf(&logEntry, "\n\033[37;1mParams: %v\033[0m\n", ctx.Params)
@@ -295,6 +296,9 @@ func (s *Server) routeHandler(req *http.Request, w http.ResponseWriter) (unused 
     //set some default headers
     ctx.SetHeader("Server", "web.go", true)
     tm := time.Now().UTC()
+
+    defer s.logRequest(ctx, tm)
+
     ctx.SetHeader("Date", webTime(tm), true)
 
     //ignore errors from ParseForm because it's usually harmless.
@@ -307,7 +311,6 @@ func (s *Server) routeHandler(req *http.Request, w http.ResponseWriter) (unused 
 
     if req.Method == "GET" || req.Method == "HEAD" {
         if s.tryServingFile(requestPath, req, w) {
-            s.LogRequest(ctx, startTime)
             return
         }
     }
@@ -354,7 +357,6 @@ func (s *Server) routeHandler(req *http.Request, w http.ResponseWriter) (unused 
             ctx.Abort(500, "Server Error")
         }
         if len(ret) == 0 {
-            s.LogRequest(ctx, startTime)
             return
         }
 
@@ -372,7 +374,6 @@ func (s *Server) routeHandler(req *http.Request, w http.ResponseWriter) (unused 
         if err != nil {
             ctx.Server.Logger.Println("Error during write: ", err)
         }
-        s.LogRequest(ctx, startTime)
         return
     }
 

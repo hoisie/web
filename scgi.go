caf1eaa2eb139b9d8eafe0bafb89a30377c4ea49
Wuvist <wuvist@gmail.com>
2014-05-10 18:10:42 +0800

    Format indent

--- scgi.go
+++ scgi.go
@@ -1,180 +1,180 @@
 package web
 
 import (
-    "bufio"
-    "bytes"
-    "errors"
-    "fmt"
-    "io"
-    "net"
-    "net/http"
-    "net/http/cgi"
-    "strconv"
-    "strings"
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/cgi"
+	"strconv"
+	"strings"
 )
 
 type scgiBody struct {
-    reader io.Reader
-    conn   io.ReadWriteCloser
-    closed bool
+	reader io.Reader
+	conn   io.ReadWriteCloser
+	closed bool
 }
 
 func (b *scgiBody) Read(p []byte) (n int, err error) {
-    if b.closed {
-        return 0, errors.New("SCGI read after close")
-    }
-    return b.reader.Read(p)
+	if b.closed {
+		return 0, errors.New("SCGI read after close")
+	}
+	return b.reader.Read(p)
 }
 
 func (b *scgiBody) Close() error {
-    b.closed = true
-    return b.conn.Close()
+	b.closed = true
+	return b.conn.Close()
 }
 
 type scgiConn struct {
-    fd           io.ReadWriteCloser
-    req          *http.Request
-    headers      http.Header
-    wroteHeaders bool
+	fd           io.ReadWriteCloser
+	req          *http.Request
+	headers      http.Header
+	wroteHeaders bool
 }
 
 func (conn *scgiConn) WriteHeader(status int) {
-    if !conn.wroteHeaders {
-        conn.wroteHeaders = true
+	if !conn.wroteHeaders {
+		conn.wroteHeaders = true
 
-        var buf bytes.Buffer
-        text := statusText[status]
+		var buf bytes.Buffer
+		text := statusText[status]
 
-        fmt.Fprintf(&buf, "HTTP/1.1 %d %s\r\n", status, text)
+		fmt.Fprintf(&buf, "HTTP/1.1 %d %s\r\n", status, text)
 
-        for k, v := range conn.headers {
-            for _, i := range v {
-                buf.WriteString(k + ": " + i + "\r\n")
-            }
-        }
+		for k, v := range conn.headers {
+			for _, i := range v {
+				buf.WriteString(k + ": " + i + "\r\n")
+			}
+		}
 
-        buf.WriteString("\r\n")
-        conn.fd.Write(buf.Bytes())
-    }
+		buf.WriteString("\r\n")
+		conn.fd.Write(buf.Bytes())
+	}
 }
 
 func (conn *scgiConn) Header() http.Header {
-    return conn.headers
+	return conn.headers
 }
 
 func (conn *scgiConn) Write(data []byte) (n int, err error) {
-    if !conn.wroteHeaders {
-        conn.WriteHeader(200)
-    }
+	if !conn.wroteHeaders {
+		conn.WriteHeader(200)
+	}
 
-    if conn.req.Method == "HEAD" {
-        return 0, errors.New("Body Not Allowed")
-    }
+	if conn.req.Method == "HEAD" {
+		return 0, errors.New("Body Not Allowed")
+	}
 
-    return conn.fd.Write(data)
+	return conn.fd.Write(data)
 }
 
 func (conn *scgiConn) Close() { conn.fd.Close() }
 
 func (conn *scgiConn) finishRequest() error {
-    var buf bytes.Buffer
-    if !conn.wroteHeaders {
-        conn.wroteHeaders = true
+	var buf bytes.Buffer
+	if !conn.wroteHeaders {
+		conn.wroteHeaders = true
 
-        for k, v := range conn.headers {
-            for _, i := range v {
-                buf.WriteString(k + ": " + i + "\r\n")
-            }
-        }
+		for k, v := range conn.headers {
+			for _, i := range v {
+				buf.WriteString(k + ": " + i + "\r\n")
+			}
+		}
 
-        buf.WriteString("\r\n")
-        conn.fd.Write(buf.Bytes())
-    }
-    return nil
+		buf.WriteString("\r\n")
+		conn.fd.Write(buf.Bytes())
+	}
+	return nil
 }
 
 func (s *Server) readScgiRequest(fd io.ReadWriteCloser) (*http.Request, error) {
-    reader := bufio.NewReader(fd)
-    line, err := reader.ReadString(':')
-    if err != nil {
-        s.Logger.Println("Error during SCGI read: ", err.Error())
-    }
-    length, _ := strconv.Atoi(line[0 : len(line)-1])
-    if length > 16384 {
-        s.Logger.Println("Error: max header size is 16k")
-    }
-    headerData := make([]byte, length)
-    _, err = reader.Read(headerData)
-    if err != nil {
-        return nil, err
-    }
+	reader := bufio.NewReader(fd)
+	line, err := reader.ReadString(':')
+	if err != nil {
+		s.Logger.Println("Error during SCGI read: ", err.Error())
+	}
+	length, _ := strconv.Atoi(line[0 : len(line)-1])
+	if length > 16384 {
+		s.Logger.Println("Error: max header size is 16k")
+	}
+	headerData := make([]byte, length)
+	_, err = reader.Read(headerData)
+	if err != nil {
+		return nil, err
+	}
 
-    b, err := reader.ReadByte()
-    if err != nil {
-        return nil, err
-    }
-    // discard the trailing comma
-    if b != ',' {
-        return nil, errors.New("SCGI protocol error: missing comma")
-    }
-    headerList := bytes.Split(headerData, []byte{0})
-    headers := map[string]string{}
-    for i := 0; i < len(headerList)-1; i += 2 {
-        headers[string(headerList[i])] = string(headerList[i+1])
-    }
-    httpReq, err := cgi.RequestFromMap(headers)
-    if err != nil {
-        return nil, err
-    }
-    if httpReq.ContentLength > 0 {
-        httpReq.Body = &scgiBody{
-            reader: io.LimitReader(reader, httpReq.ContentLength),
-            conn:   fd,
-        }
-    } else {
-        httpReq.Body = &scgiBody{reader: reader, conn: fd}
-    }
-    return httpReq, nil
+	b, err := reader.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+	// discard the trailing comma
+	if b != ',' {
+		return nil, errors.New("SCGI protocol error: missing comma")
+	}
+	headerList := bytes.Split(headerData, []byte{0})
+	headers := map[string]string{}
+	for i := 0; i < len(headerList)-1; i += 2 {
+		headers[string(headerList[i])] = string(headerList[i+1])
+	}
+	httpReq, err := cgi.RequestFromMap(headers)
+	if err != nil {
+		return nil, err
+	}
+	if httpReq.ContentLength > 0 {
+		httpReq.Body = &scgiBody{
+			reader: io.LimitReader(reader, httpReq.ContentLength),
+			conn:   fd,
+		}
+	} else {
+		httpReq.Body = &scgiBody{reader: reader, conn: fd}
+	}
+	return httpReq, nil
 }
 
 func (s *Server) handleScgiRequest(fd io.ReadWriteCloser) {
-    req, err := s.readScgiRequest(fd)
-    if err != nil {
-        s.Logger.Println("SCGI error: %q", err.Error())
-    }
-    sc := scgiConn{fd, req, make(map[string][]string), false}
-    s.routeHandler(req, &sc)
-    sc.finishRequest()
-    fd.Close()
+	req, err := s.readScgiRequest(fd)
+	if err != nil {
+		s.Logger.Println("SCGI error: %q", err.Error())
+	}
+	sc := scgiConn{fd, req, make(map[string][]string), false}
+	s.routeHandler(req, &sc)
+	sc.finishRequest()
+	fd.Close()
 }
 
 func (s *Server) listenAndServeScgi(addr string) error {
 
-    var l net.Listener
-    var err error
+	var l net.Listener
+	var err error
 
-    //if the path begins with a "/", assume it's a unix address
-    if strings.HasPrefix(addr, "/") {
-        l, err = net.Listen("unix", addr)
-    } else {
-        l, err = net.Listen("tcp", addr)
-    }
+	//if the path begins with a "/", assume it's a unix address
+	if strings.HasPrefix(addr, "/") {
+		l, err = net.Listen("unix", addr)
+	} else {
+		l, err = net.Listen("tcp", addr)
+	}
 
-    //save the listener so it can be closed
-    s.l = l
+	//save the listener so it can be closed
+	s.l = l
 
-    if err != nil {
-        s.Logger.Println("SCGI listen error", err.Error())
-        return err
-    }
+	if err != nil {
+		s.Logger.Println("SCGI listen error", err.Error())
+		return err
+	}
 
-    for {
-        fd, err := l.Accept()
-        if err != nil {
-            s.Logger.Println("SCGI accept error", err.Error())
-            return err
-        }
-        go s.handleScgiRequest(fd)
-    }
-    return nil
+	for {
+		fd, err := l.Accept()
+		if err != nil {
+			s.Logger.Println("SCGI accept error", err.Error())
+			return err
+		}
+		go s.handleScgiRequest(fd)
+	}
+	return nil
 }
--- web.go
+++ web.go
@@ -3,23 +3,23 @@
 package web
 
 import (
-    "bytes"
-    "code.google.com/p/go.net/websocket"
-    "crypto/hmac"
-    "crypto/sha1"
-    "crypto/tls"
-    "encoding/base64"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "mime"
-    "net/http"
-    "os"
-    "path"
-    "reflect"
-    "strconv"
-    "strings"
-    "time"
+	"bytes"
+	"code.google.com/p/go.net/websocket"
+	"crypto/hmac"
+	"crypto/sha1"
+	"crypto/tls"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // A Context object is created for every incoming HTTP request, and is
@@ -27,15 +27,15 @@ import (
 // about the request, including the http.Request object, the GET and POST params,
 // and acts as a Writer for the response.
 type Context struct {
-    Request *http.Request
-    Params  map[string]string
-    Server  *Server
-    http.ResponseWriter
+	Request *http.Request
+	Params  map[string]string
+	Server  *Server
+	http.ResponseWriter
 }
 
 // WriteString writes string data into the response object.
 func (ctx *Context) WriteString(content string) {
-    ctx.ResponseWriter.Write([]byte(content))
+	ctx.ResponseWriter.Write([]byte(content))
 }
 
 // Abort is a helper method that sends an HTTP header and an optional
@@ -43,36 +43,36 @@ func (ctx *Context) WriteString(content string) {
 // Once it has been called, any return value from the handler will
 // not be written to the response.
 func (ctx *Context) Abort(status int, body string) {
-    ctx.ResponseWriter.WriteHeader(status)
-    ctx.ResponseWriter.Write([]byte(body))
+	ctx.ResponseWriter.WriteHeader(status)
+	ctx.ResponseWriter.Write([]byte(body))
 }
 
 // Redirect is a helper method for 3xx redirects.
 func (ctx *Context) Redirect(status int, url_ string) {
-    ctx.ResponseWriter.Header().Set("Location", url_)
-    ctx.ResponseWriter.WriteHeader(status)
-    ctx.ResponseWriter.Write([]byte("Redirecting to: " + url_))
+	ctx.ResponseWriter.Header().Set("Location", url_)
+	ctx.ResponseWriter.WriteHeader(status)
+	ctx.ResponseWriter.Write([]byte("Redirecting to: " + url_))
 }
 
 // Notmodified writes a 304 HTTP response
 func (ctx *Context) NotModified() {
-    ctx.ResponseWriter.WriteHeader(304)
+	ctx.ResponseWriter.WriteHeader(304)
 }
 
 // NotFound writes a 404 HTTP response
 func (ctx *Context) NotFound(message string) {
-    ctx.ResponseWriter.WriteHeader(404)
-    ctx.ResponseWriter.Write([]byte(message))
+	ctx.ResponseWriter.WriteHeader(404)
+	ctx.ResponseWriter.Write([]byte(message))
 }
 
 //Unauthorized writes a 401 HTTP response
 func (ctx *Context) Unauthorized() {
-    ctx.ResponseWriter.WriteHeader(401)
+	ctx.ResponseWriter.WriteHeader(401)
 }
 
 //Forbidden writes a 403 HTTP response
 func (ctx *Context) Forbidden() {
-    ctx.ResponseWriter.WriteHeader(403)
+	ctx.ResponseWriter.WriteHeader(403)
 }
 
 // ContentType sets the Content-Type header for an HTTP response.
@@ -81,93 +81,93 @@ func (ctx *Context) Forbidden() {
 // verbatim. The return value is the content type as it was
 // set, or an empty string if none was found.
 func (ctx *Context) ContentType(val string) string {
-    var ctype string
-    if strings.ContainsRune(val, '/') {
-        ctype = val
-    } else {
-        if !strings.HasPrefix(val, ".") {
-            val = "." + val
-        }
-        ctype = mime.TypeByExtension(val)
-    }
-    if ctype != "" {
-        ctx.Header().Set("Content-Type", ctype)
-    }
-    return ctype
+	var ctype string
+	if strings.ContainsRune(val, '/') {
+		ctype = val
+	} else {
+		if !strings.HasPrefix(val, ".") {
+			val = "." + val
+		}
+		ctype = mime.TypeByExtension(val)
+	}
+	if ctype != "" {
+		ctx.Header().Set("Content-Type", ctype)
+	}
+	return ctype
 }
 
 // SetHeader sets a response header. If `unique` is true, the current value
 // of that header will be overwritten . If false, it will be appended.
 func (ctx *Context) SetHeader(hdr string, val string, unique bool) {
-    if unique {
-        ctx.Header().Set(hdr, val)
-    } else {
-        ctx.Header().Add(hdr, val)
-    }
+	if unique {
+		ctx.Header().Set(hdr, val)
+	} else {
+		ctx.Header().Add(hdr, val)
+	}
 }
 
 // SetCookie adds a cookie header to the response.
 func (ctx *Context) SetCookie(cookie *http.Cookie) {
-    ctx.SetHeader("Set-Cookie", cookie.String(), false)
+	ctx.SetHeader("Set-Cookie", cookie.String(), false)
 }
 
 func getCookieSig(key string, val []byte, timestamp string) string {
-    hm := hmac.New(sha1.New, []byte(key))
+	hm := hmac.New(sha1.New, []byte(key))
 
-    hm.Write(val)
-    hm.Write([]byte(timestamp))
+	hm.Write(val)
+	hm.Write([]byte(timestamp))
 
-    hex := fmt.Sprintf("%02x", hm.Sum(nil))
-    return hex
+	hex := fmt.Sprintf("%02x", hm.Sum(nil))
+	return hex
 }
 
 func (ctx *Context) SetSecureCookie(name string, val string, age int64) {
-    //base64 encode the val
-    if len(ctx.Server.Config.CookieSecret) == 0 {
-        ctx.Server.Logger.Println("Secret Key for secure cookies has not been set. Please assign a cookie secret to web.Config.CookieSecret.")
-        return
-    }
-    var buf bytes.Buffer
-    encoder := base64.NewEncoder(base64.StdEncoding, &buf)
-    encoder.Write([]byte(val))
-    encoder.Close()
-    vs := buf.String()
-    vb := buf.Bytes()
-    timestamp := strconv.FormatInt(time.Now().Unix(), 10)
-    sig := getCookieSig(ctx.Server.Config.CookieSecret, vb, timestamp)
-    cookie := strings.Join([]string{vs, timestamp, sig}, "|")
-    ctx.SetCookie(NewCookie(name, cookie, age))
+	//base64 encode the val
+	if len(ctx.Server.Config.CookieSecret) == 0 {
+		ctx.Server.Logger.Println("Secret Key for secure cookies has not been set. Please assign a cookie secret to web.Config.CookieSecret.")
+		return
+	}
+	var buf bytes.Buffer
+	encoder := base64.NewEncoder(base64.StdEncoding, &buf)
+	encoder.Write([]byte(val))
+	encoder.Close()
+	vs := buf.String()
+	vb := buf.Bytes()
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	sig := getCookieSig(ctx.Server.Config.CookieSecret, vb, timestamp)
+	cookie := strings.Join([]string{vs, timestamp, sig}, "|")
+	ctx.SetCookie(NewCookie(name, cookie, age))
 }
 
 func (ctx *Context) GetSecureCookie(name string) (string, bool) {
-    for _, cookie := range ctx.Request.Cookies() {
-        if cookie.Name != name {
-            continue
-        }
+	for _, cookie := range ctx.Request.Cookies() {
+		if cookie.Name != name {
+			continue
+		}
 
-        parts := strings.SplitN(cookie.Value, "|", 3)
+		parts := strings.SplitN(cookie.Value, "|", 3)
 
-        val := parts[0]
-        timestamp := parts[1]
-        sig := parts[2]
+		val := parts[0]
+		timestamp := parts[1]
+		sig := parts[2]
 
-        if getCookieSig(ctx.Server.Config.CookieSecret, []byte(val), timestamp) != sig {
-            return "", false
-        }
+		if getCookieSig(ctx.Server.Config.CookieSecret, []byte(val), timestamp) != sig {
+			return "", false
+		}
 
-        ts, _ := strconv.ParseInt(timestamp, 0, 64)
+		ts, _ := strconv.ParseInt(timestamp, 0, 64)
 
-        if time.Now().Unix()-31*86400 > ts {
-            return "", false
-        }
+		if time.Now().Unix()-31*86400 > ts {
+			return "", false
+		}
 
-        buf := bytes.NewBufferString(val)
-        encoder := base64.NewDecoder(base64.StdEncoding, buf)
+		buf := bytes.NewBufferString(val)
+		encoder := base64.NewDecoder(base64.StdEncoding, buf)
 
-        res, _ := ioutil.ReadAll(encoder)
-        return string(res), true
-    }
-    return "", false
+		res, _ := ioutil.ReadAll(encoder)
+		return string(res), true
+	}
+	return "", false
 }
 
 // small optimization: cache the context type instead of repeteadly calling reflect.Typeof
@@ -176,96 +176,96 @@ var contextType reflect.Type
 var defaultStaticDirs []string
 
 func init() {
-    contextType = reflect.TypeOf(Context{})
-    //find the location of the exe file
-    wd, _ := os.Getwd()
-    arg0 := path.Clean(os.Args[0])
-    var exeFile string
-    if strings.HasPrefix(arg0, "/") {
-        exeFile = arg0
-    } else {
-        //TODO for robustness, search each directory in $PATH
-        exeFile = path.Join(wd, arg0)
-    }
-    parent, _ := path.Split(exeFile)
-    defaultStaticDirs = append(defaultStaticDirs, path.Join(parent, "static"))
-    defaultStaticDirs = append(defaultStaticDirs, path.Join(wd, "static"))
-    return
+	contextType = reflect.TypeOf(Context{})
+	//find the location of the exe file
+	wd, _ := os.Getwd()
+	arg0 := path.Clean(os.Args[0])
+	var exeFile string
+	if strings.HasPrefix(arg0, "/") {
+		exeFile = arg0
+	} else {
+		//TODO for robustness, search each directory in $PATH
+		exeFile = path.Join(wd, arg0)
+	}
+	parent, _ := path.Split(exeFile)
+	defaultStaticDirs = append(defaultStaticDirs, path.Join(parent, "static"))
+	defaultStaticDirs = append(defaultStaticDirs, path.Join(wd, "static"))
+	return
 }
 
 // Process invokes the main server's routing system.
 func Process(c http.ResponseWriter, req *http.Request) {
-    mainServer.Process(c, req)
+	mainServer.Process(c, req)
 }
 
 // Run starts the web application and serves HTTP requests for the main server.
 func Run(addr string) {
-    mainServer.Run(addr)
+	mainServer.Run(addr)
 }
 
 // RunTLS starts the web application and serves HTTPS requests for the main server.
 func RunTLS(addr string, config *tls.Config) {
-    mainServer.RunTLS(addr, config)
+	mainServer.RunTLS(addr, config)
 }
 
 // RunScgi starts the web application and serves SCGI requests for the main server.
 func RunScgi(addr string) {
-    mainServer.RunScgi(addr)
+	mainServer.RunScgi(addr)
 }
 
 // RunFcgi starts the web application and serves FastCGI requests for the main server.
 func RunFcgi(addr string) {
-    mainServer.RunFcgi(addr)
+	mainServer.RunFcgi(addr)
 }
 
 // Close stops the main server.
 func Close() {
-    mainServer.Close()
+	mainServer.Close()
 }
 
 // Get adds a handler for the 'GET' http method in the main server.
 func Get(route string, handler interface{}) {
-    mainServer.Get(route, handler)
+	mainServer.Get(route, handler)
 }
 
 // Post adds a handler for the 'POST' http method in the main server.
 func Post(route string, handler interface{}) {
-    mainServer.addRoute(route, "POST", handler)
+	mainServer.addRoute(route, "POST", handler)
 }
 
 // Put adds a handler for the 'PUT' http method in the main server.
 func Put(route string, handler interface{}) {
-    mainServer.addRoute(route, "PUT", handler)
+	mainServer.addRoute(route, "PUT", handler)
 }
 
 // Delete adds a handler for the 'DELETE' http method in the main server.
 func Delete(route string, handler interface{}) {
-    mainServer.addRoute(route, "DELETE", handler)
+	mainServer.addRoute(route, "DELETE", handler)
 }
 
 // Match adds a handler for an arbitrary http method in the main server.
 func Match(method string, route string, handler interface{}) {
-    mainServer.addRoute(route, method, handler)
+	mainServer.addRoute(route, method, handler)
 }
 
 //Adds a custom handler. Only for webserver mode. Will have no effect when running as FCGI or SCGI.
 func Handler(route string, method string, httpHandler http.Handler) {
-    mainServer.Handler(route, method, httpHandler)
+	mainServer.Handler(route, method, httpHandler)
 }
 
 //Adds a handler for websockets. Only for webserver mode. Will have no effect when running as FCGI or SCGI.
 func Websocket(route string, httpHandler websocket.Handler) {
-    mainServer.Websocket(route, httpHandler)
+	mainServer.Websocket(route, httpHandler)
 }
 
 // SetLogger sets the logger for the main server.
 func SetLogger(logger *log.Logger) {
-    mainServer.Logger = logger
+	mainServer.Logger = logger
 }
 
 // Config is the configuration of the main server.
 var Config = &ServerConfig{
-    RecoverPanic: true,
+	RecoverPanic: true,
 }
 
 var mainServer = NewServer()

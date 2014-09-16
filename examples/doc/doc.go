48c63d43e136854e2eb8bd3fb535c810839e6956
Alexander F Rødseth <rodseth@gmail.com>
2014-09-16 15:46:13 +0200

    Removed empty example

--- examples/doc/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright © 2009--2014 The Web.go Authors
-// Use of this source code is governed by a MIT-style
-// license that can be found in the LICENSE file.
-
-// Sample applications in web.go
-package main

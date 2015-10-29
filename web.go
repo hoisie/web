fb44be79ed8e714de1f9e576e9c3fd8a0895602b
xiaobeic <756048962@qq.com>
2015-10-29 18:07:21 +0800

    only test

--- web.go
+++ web.go
@@ -263,9 +263,9 @@ func SetLogger(logger *log.Logger) {
     mainServer.Logger = logger
 }
 
-// Config is the configuration of the main server.
+// Config is the configuration of the main server..
 var Config = &ServerConfig{
     RecoverPanic: true,
-}
+} 
 
 var mainServer = NewServer()
